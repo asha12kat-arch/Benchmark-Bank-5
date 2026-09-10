8bdc234783d48550598476d99c17803adc8112ed
asha12kat-arch <asha12kat@gmail.com>
2026-09-10 20:05:53 +0100

    Update CardsScreen.tsx

--- src/screens/CardsScreen.tsx
+++ src/screens/CardsScreen.tsx
@@ -32,7 +32,7 @@ export default function CardsScreen({ navigate }: Props) {
               <div className="content-stretch flex items-start justify-between relative shrink-0 w-full">
                 <div className="content-stretch flex flex-col gap-[2px] items-start relative shrink-0">
                   <p className="font-['Geist:Regular',sans-serif] font-normal relative shrink-0 text-[9px] text-[rgba(255,255,255,0.5)] uppercase">Cardholder</p>
-                 <p className="font-['Geist:SemiBold',sans-serif] font-semibold leading-[normal] relative shrink-0 text-[13px] text-white">MICHAEL DOUGLAS SKINNER</p>
+                 <p className="font-['Geist:SemiBold',sans-serif] font-semibold leading-[normal] relative shrink-0 text-[13px] text-white">Leland Lee Tilson</p>
                 </div>
                 <div className="content-stretch flex flex-col gap-[2px] items-end relative shrink-0">
                   <p className="font-['Geist:Regular',sans-serif] font-normal relative shrink-0 text-[9px] text-[rgba(255,255,255,0.5)] uppercase">Expires</p>
